6fd37cf23ab90ac5c40f903b24910a2ac9d58435
Mark Christian <chcknknight@gmail.com>
2023-11-20 17:57:12 -0500

    dumb sidebar

--- app/page.js
+++ app/page.js
@@ -1,27 +1,7 @@
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import Link from 'next/link';
-
 export default function Home() {
   return (
     <div>
-      {Bar()}
       <strong>Title Place holder</strong>
     </div>
-  );
-}
-
-function Bar() {
-  return (
-    <Sidebar>
-      <Menu>
-        <SubMenu label="Personal">
-          <MenuItem>About Me</MenuItem>
-          <MenuItem>Resume</MenuItem>
-        </SubMenu>
-        <SubMenu label="Projects">
-          <MenuItem component={<Link href="/data101"/>}>Data 101</MenuItem>
-        </SubMenu>
-      </Menu>
-    </Sidebar>
   );
 }
