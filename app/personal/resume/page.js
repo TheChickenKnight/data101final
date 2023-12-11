25dc4c24f0adc92b160d983d256384b186f0d458
TheChickenKnight <81781990+TheChickenKnight@users.noreply.github.com>
2023-12-11 12:23:02 -0800

    resume displayer

--- app/personal/resume/page.js
+++ app/personal/resume/page.js
@@ -8,9 +8,7 @@ export const data = {
 
 export default function Resume() {
     return (
-      <div>
-        <strong>Title Place holder</strong>
-      </div>
+        <iframe className="w-full h-full" src="https://docs.google.com/gview?url=https://github.com/TheChickenKnight/data101final/raw/master/public/Mark%20Christian%20Resume.docx&embedded=true"/>
     );
   }
   
