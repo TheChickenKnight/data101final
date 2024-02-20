3f6ed3e97a15abe64d3a131ee7f40e85d3db9a8e
Mark Christian <chcknknight@gmail.com>
2024-02-20 17:43:03 -0500

    button fixes

--- components/ui/DarkModeButton.jsx
+++ components/ui/DarkModeButton.jsx
@@ -5,14 +5,17 @@ import DarkIcon from "../icons/DarkIcon"
 
 export default function DarkModeButton({classy, ...props}) {
     function darkMode() {
-        console.log(classy)
         let body = document.getElementById("body");
-        if (body.classList.contains("dark")) 
+        let button = document.getElementById("drkicon");
+        if (body.classList.contains("dark"))  {
             body.classList.remove("dark");
-        else
+            button.outerHTML = button.outerHTML.replace(/fill="#ffffff"/g, "fill=\"#1C274C\"")
+        } else {
             body.classList.add("dark");
+            button.outerHTML = button.outerHTML.replace(/fill="#1C274C"/g, "fill=\"#ffffff\"")
+        }
     }
     return (
-        <Button {...props} onClick={() => darkMode()} className={"fixed z-50 bottom-5 right-5 p-4 dark:bg-slate-800 bg-slate-400" + classy}><DarkIcon className="stroke-black dark:stroke-white"/></Button>
+        <Button {...props} id="dkMdBtn" onClick={darkMode} className={"fixed z-50 bottom-5 right-5 p-4 dark:bg-slate-800 bg-slate-400" + classy}><DarkIcon id="drkicon" className="stroke-white dark:stroke-black"/></Button>
     )
 }
