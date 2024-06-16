07f5ce84b29114d923531aaf74ffa303936ce86e
TheChickenKnight <81781990+TheChickenKnight@users.noreply.github.com>
2024-06-16 07:35:58 -0400

    resume change

--- app/personal/resume/page.js
+++ app/personal/resume/page.js
@@ -8,7 +8,7 @@ const data = {
 
 export default function Resume() {
     return (
-        <iframe className="w-full h-full" src="https://docs.google.com/gview?url=https://chckn.vercel.app/resume.docx"/>
+        <iframe className="w-full h-full" src="/resume.html"/>
     );
   }
   
