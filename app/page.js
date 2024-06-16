9412be0b51db70b90dee618ca68c26d18414635b
TheChickenKnight <81781990+TheChickenKnight@users.noreply.github.com>
2024-06-16 07:00:58 -0400

    home change

--- app/page.js
+++ app/page.js
@@ -9,8 +9,8 @@ export default async function Home() {
   return (
     <div>
       <p className="text-9xl text-black dark:text-white"><strong>Chckn.Vercel</strong></p>
-      <p className="text-slate-600 dark:text-slate-200">Official website of Mark Christian (TheChickenKnight)</p>
-      <a href="https://steamcommunity.com/profiles/76561198420907011" target="_blank"><SteamIcon className="inline mt-1 hover:fill-indigo-950 dark:hover:fill-zinc-300 hover:mt-0 duration-75"/></a>
+      <p className="text-slate-600 dark:text-slate-200 ml-4">Official website of Mark Christian (TheChickenKnight)</p>
+      <a href="https://steamcommunity.com/profiles/76561198420907011" target="_blank"><SteamIcon className="inline mt-1 hover:fill-indigo-950 dark:hover:fill-zinc-300 hover:mt-0 duration-75 ml-4"/></a>
       <a href="https://open.spotify.com/user/4s7g7m6go7nt6fcu1wi6ldqaz?si=ZKyJGMvYR2KIyihNpfiMbQ" target="_blank"><SpotifyIcon className="inline mt-1 ml-2 hover:fill-green-500 hover:mt-0 duration-0"/></a>
       <a href="https://github.com/TheChickenKnight" target="_blank"><GithubIcon className="inline mt-1 ml-2 hover:fill-black dark:hover:fill-white hover:mt-0 duration-0"/></a>
       <Spotify/>
