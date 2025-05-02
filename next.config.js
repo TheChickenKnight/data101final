d0eab0f23ccbc0a7f9bcd62a4a4f64557fab0927
TheChickenKnight <chcknknight@gmail.com>
2025-05-02 02:06:29 -0400

    remove sentry

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ['gpti'],
    webpack: (config, { isServer }) => {
       if (!isServer) {
         config.resolve.fallback = { ["node:buffer"]: false };
